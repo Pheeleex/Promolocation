8402ac178a5a36486a6af1da850bfb973baba738
Nubiaville <nubiaville@Nubiavilles-MacBook-Pro-2.local>
2026-02-18 09:17:59 +0100

    serve

--- js/promoters-logic.js
+++ js/promoters-logic.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   const authUser = localStorage.getItem("authUser");
 
   if (!authUser) {
-    window.location.href = "Auth.html";
+    window.location.href = "auth.html";
   }
   // 1. DATA AND INITIALIZATION
   const promoters = [
